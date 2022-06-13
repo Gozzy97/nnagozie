058d886be6ae07df395f886e1a3a13e60ff555fa
Emmanuel Nnagozie Eke <60792291+Gozzy97@users.noreply.github.com>
2022-06-13 15:30:21 +0100

    Delete deleteafternow.py

--- Reading-Text-Files/deleteafternow.py
+++
@@ -1,6 +0,0 @@
-text = 'Once', 'upon', 'a', 'time', 'a', 'psychology', 'professor', 'walked', 'around', 'on', 'a', 'stage', 'while', 'teaching', 'stress', 'management', 'principles', 'to', 'an', 'auditorium', 'filled', 'with', 'students'
-for i in text:
-    num = text.count(i)
-    
-print ("yes you got it")
-print(num)
